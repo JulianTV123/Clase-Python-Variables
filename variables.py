094a95bb25a29970e3fbc6331acc821d9120b7ab
JulianTocasuche <julistv1234@gmail.com>
2024-08-16 12:58:45 -0500

    Initial Commit

---
+++ variables.py
@@ -0,0 +1,30 @@
+# Variables en Python
+
+# Declaracion e inicializacion de variables
+edad = 30
+altura = 1.65
+pais = "Camboya"
+
+# Acceder a las variables
+print("Valores iniciales: ")
+print("Edad:", edad)
+print("Altura:", altura)
+print("Pais:", pais)
+
+# Modificar el valor de una variable
+edad = 15
+altura = 1.90
+pais = "China"
+
+print("Valores modificados: ")
+print("Edad:", edad)
+print("Altura:", altura)
+print("Pais:", pais)
+
+# En Python el tipo es dinamico
+edad = "quince"
+print("Edad:", edad)
+
+# Si queremos acceder a una variable no declarada manda error
+telefono = "3005121049"
+print("Telefono:", telefono)
